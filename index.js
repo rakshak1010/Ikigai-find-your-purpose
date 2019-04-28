1c47603b43a87cb32b11bac00fb0849212e43539
Rakshak <42585240+rakshak1010@users.noreply.github.com>
2019-04-28 20:09:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 
 
 var mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/rak", { useNewUrlParser: true });
+mongoose.connect("mongodb://sid:sid123@ds127825.mlab.com:27825/intern", { useNewUrlParser: true });
 mongoose.connection.on('error',function(error){
   console.error("Error Occured in DB",error);
 });
